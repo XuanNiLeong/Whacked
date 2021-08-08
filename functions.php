e0978e474b71c4a9426f5a633489f3a01fbbaa3b
XuanNiLeong <lxuanniii@gmail.com>
2021-08-08 19:25:12 +0100

    initial commit

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+// Add dynamic title tag support
+function whackedcode_theme_support(){
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'whackedcode_theme_support');
+
+// register css styles
+function whackedcode_register_styles(){
+
+    wp_enqueue_style('whackedcode-style', get_template_directory_uri() ."/style.css", array('whackedcode-bootstrap'), '1.0','all');
+    wp_enqueue_style('whackedcode-bootstrap', "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", array(), '1.0','all');
+    wp_enqueue_style('whackedcode-cloudflare', "https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.css", array(), '1.0','all');
+    wp_enqueue_style('whackedcode-googleapis', "https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap", array(), '1.0','all');
+    wp_enqueue_style('whackedcode-github', "https://md-aqil.github.io/images/swiper.min.css", array(), '1.0','all');
+
+}
+
+add_action('wp_enqueue_scripts', 'whackedcode_register_styles');
+
+
+// register js
+function whackedcode_register_scripts(){
+
+    wp_enqueue_script( 'whackedcode-boostrap', "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-cloudflare', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-jquery', "https://code.jquery.com/jquery-3.2.1.slim.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-github', "https://md-aqil.github.io/images/swiper.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-js', get_template_directory_uri() ."/assets/js/main.js", array(), '1.0', 'true');
+
+
+}
+
+add_action('wp_enqueue_scripts', 'whackedcode_register_scripts');
+
+?>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Meta  -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php
+    wp_head();
+    ?>
+
+  </head>
+
+  <body>
+    
+  <header> 
+
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <a class="navbar-brand" href="/wp-content/themes/whacked/code/index.html"><img src="/wp-content/themes/whacked/code/assets/images/logo.png" alt="logo" width="55px" height="55px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-lg-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/wp-content/themes/whacked/code/index.html">About <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/wp-content/themes/whacked/code/team.html">Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/wp-content/themes/whacked/code/contact.html">Contact</a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link" href="#">Free consultation</a>
+        </li> 
+      </ul>
+    </div>
+  </nav>
+</div>
+
+</header>
