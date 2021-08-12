45c316bddd5cf4d25ec74b515811c404b676ca28
XuanNiLeong <lxuanniii@gmail.com>
2021-08-12 17:41:34 +0100

    updated illustrations

--- functions.php
+++ functions.php
@@ -24,9 +24,9 @@ add_action('wp_enqueue_scripts', 'whackedcode_register_styles');
 // register js
 function whackedcode_register_scripts(){
 
-    wp_enqueue_script( 'whackedcode-boostrap', "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", array(), '1.0', 'true');
-    wp_enqueue_script( 'whackedcode-cloudflare', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", array(), '1.0', 'true');
     wp_enqueue_script( 'whackedcode-jquery', "https://code.jquery.com/jquery-3.2.1.slim.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-cloudflare', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", array(), '1.0', 'true');
+    wp_enqueue_script( 'whackedcode-boostrap', "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", array(), '1.0', 'true');
     wp_enqueue_script( 'whackedcode-github', "https://md-aqil.github.io/images/swiper.min.js", array(), '1.0', 'true');
     wp_enqueue_script( 'whackedcode-js', get_template_directory_uri() ."/assets/js/main.js", array(), '1.0', 'true');
 
