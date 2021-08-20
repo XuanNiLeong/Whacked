d692e9c3c8bcc144f107e382862ee3dfff16f228
XuanNiLeong <lxuanniii@gmail.com>
2021-08-20 10:40:01 +0100

    Wordpress commit

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php 
+
+			//submit the form
+			$userName = $_POST['name'];
+			$userEmail = $_POST['email'];
+			$message = $_POST['subject'];
+
+      $email_from = 'lxuanniii@gmail.com';
+      $email_subject = "Enquiries";
+      $email_body = "Name: $userName.\n".
+			              "Email: $userEmail.\n".
+			             "Message: $message.\n";
+
+			$to = "lxuanniii@gmail.com";
+			$headers = "From: $email_from \r\n";
+      $headers .= "Reply-To: $userEmail \r\n";
+
+			mail($to, $email_subject, $email_body, $headers);
+
+      header("Location: contact.html");
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+?> 
